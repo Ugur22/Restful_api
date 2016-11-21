3a9844afc0786e58b433536f185e984be3a56ec7
Ugur <ugurertas9@gmail.com>
2016-11-21 13:53:19 +0100

    project

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+
+var db = mongoose.connect('mongodb://localhost:27017/bookAPI');
+
+var Book = require('./models/bookModel');
+
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+bookRouter = require("./Routes/bookRoutes")(Book);
+
+
+app.use('/api/books', bookRouter);
+// app.use('/api/authors', authorRouter);
+
+app.get('/', function (req, res) {
+    res.send('welcome to my Restful WEB API');
+});
+
+app.listen(port, function () {
+    console.log('running on Port:' + port);
+});
+
