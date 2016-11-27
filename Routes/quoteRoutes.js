30bd8e0c0cb66afddd058dba9a11646fe3e9fb86
Ugur <ugurertas9@gmail.com>
2016-11-27 18:08:11 +0100

    updated detail added link to collection

--- Routes/quoteRoutes.js
+++ Routes/quoteRoutes.js
@@ -30,7 +30,9 @@ var routes = function (Quote) {
             var returnQuote = req.quote.toJSON();
             returnQuote._links = {};
             returnQuote._links.self = {};
-            returnQuote._links.self.href = 'http://' + req.headers.host + '/api/quotes/';
+            returnQuote._links.self.href = 'http://' + req.headers.host + '/api/quotes/' + returnQuote._id;
+            returnQuote._links.collection = {};
+            returnQuote._links.collection.href = 'http://' + req.headers.host + '/api/quotes/';
             res.json(returnQuote);
         })
         .put(function (req, res) {
