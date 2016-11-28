3b0f134cb33f898b6709b2e9f0faee7b9b39b3ee
Ugur <ugurertas9@gmail.com>
2016-11-28 10:35:06 +0100

    refactored crud functions and routes

--- Controllers/quoteController.js
+++ Controllers/quoteController.js
@@ -77,10 +77,88 @@ var quoteController = function (Quote) {
         });
     };
 
+
+    var getSingleQuote = function (req, res) {
+        var returnQuote = req.quote.toJSON();
+        returnQuote._links = {};
+        returnQuote._links.self = {};
+        returnQuote._links.self.href = 'http://' + req.headers.host + '/api/quotes/' + returnQuote._id;
+        returnQuote._links.collection = {};
+        returnQuote._links.collection.href = 'http://' + req.headers.host + '/api/quotes/';
+        res.json(returnQuote);
+
+    };
+
+    var singleQuoteOptions = function (req, res) {
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE, OPTIONS');
+        res.end();
+    };
+
+    var putQuote = function (req, res) {
+        req.quote.text = req.body.text;
+        req.quote.author = req.body.author;
+        req.quote.tag = req.body.tag;
+
+        if (!req.body.text) {
+            res.status(400);
+            res.send('text is required');
+        }
+        else if (!req.body.author) {
+            res.status(400);
+            res.send('author is required');
+        }
+
+        else if (!req.body.tag) {
+            res.status(400);
+            res.send('tag is required');
+        }
+        else {
+            req.quote.save(function (err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.quote);
+                }
+            });
+        }
+    };
+
+    var patchQuote = function (req, res) {
+        if (req.body._id) {
+            delete req.body._id;
+        }
+        for (var p in req.body) {
+            req.quote[p] = req.body[p];
+        }
+
+        req.quote.save(function (err) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(req.quote);
+            }
+        });
+    };
+
+    var deleteQuote = function (req, res) {
+        req.quote.remove(function (err) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(204).send('Removed');
+            }
+        });
+    };
+
     return {
         post: post,
         get: get,
-        options: options
+        options: options,
+        getSingleQuote: getSingleQuote,
+        singleQuoteOptions: singleQuoteOptions,
+        putQuote: putQuote,
+        patchQuote: patchQuote,
+        deleteQuote: deleteQuote
     }
 };
 
--- Routes/quoteRoutes.js
+++ Routes/quoteRoutes.js
@@ -25,73 +25,11 @@ var routes = function (Quote) {
     });
 
     quoteRouter.route('/:quoteId')
-        .get(function (req, res) {
-            var returnQuote = req.quote.toJSON();
-            returnQuote._links = {};
-            returnQuote._links.self = {};
-            returnQuote._links.self.href = 'http://' + req.headers.host + '/api/quotes/' + returnQuote._id;
-            returnQuote._links.collection = {};
-            returnQuote._links.collection.href = 'http://' + req.headers.host + '/api/quotes/';
-            res.json(returnQuote);
-        })
-        .options(function (req, res) {
-            res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE, OPTIONS');
-            res.end();
-        })
-        .put(function (req, res) {
-            req.quote.text = req.body.text;
-            req.quote.author = req.body.author;
-            req.quote.tag = req.body.tag;
-
-            if (!req.body.text) {
-                res.status(400);
-                res.send('text is required');
-            }
-            else if (!req.body.author) {
-                res.status(400);
-                res.send('author is required');
-            }
-
-            else if (!req.body.tag) {
-                res.status(400);
-                res.send('tag is required');
-            }
-            else {
-                req.quote.save(function (err) {
-                    if (err) {
-                        res.status(500).send(err);
-                    } else {
-                        res.json(req.quote);
-                    }
-                });
-            }
-
-        })
-        .patch(function (req, res) {
-            if (req.body._id) {
-                delete req.body._id;
-            }
-            for (var p in req.body) {
-                req.quote[p] = req.body[p];
-            }
-
-            req.quote.save(function (err) {
-                if (err) {
-                    res.status(500).send(err);
-                } else {
-                    res.json(req.quote);
-                }
-            });
-        })
-        .delete(function (req, res) {
-            req.quote.remove(function (err) {
-                if (err) {
-                    res.status(500).send(err);
-                } else {
-                    res.status(204).send('Removed');
-                }
-            });
-        });
+        .get(quoteController.getSingleQuote)
+        .options(quoteController.singleQuoteOptions)
+        .put(quoteController.putQuote)
+        .patch(quoteController.patchQuote)
+        .delete(quoteController.deleteQuote);
     return quoteRouter;
 };
 
