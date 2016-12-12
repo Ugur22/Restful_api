956fe71e2b268c5d75bd3fe09d2afae87807f1a1
Ugur <ugurertas9@gmail.com>
2016-12-12 14:22:44 +0100

    retry

--- app.js
+++ app.js
@@ -14,9 +14,8 @@ var port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(function (res, req, next) {
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-    res.header('Access-Control-Allow-Origin', '*');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
